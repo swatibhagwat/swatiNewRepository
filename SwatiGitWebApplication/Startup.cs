e0217154c98554028213f3e71e9b076ded7de2d4
swatibhagwat <swatibhagwat123@gmail.com>
2014-05-08 14:22:32 +0530

    FirstCommit

---
+++ SwatiGitWebApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwatiGitWebApplication.Startup))]
+namespace SwatiGitWebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
